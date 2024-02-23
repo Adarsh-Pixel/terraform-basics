196400db4ef78bbb6a6524e58ccb48174aa66b48
unknown <kumaradarsh921@gmail.com>
2024-02-23 14:16:41 +0530

    Git Auto push

--- 02-vars/demo.tf
+++ 02-vars/demo.tf
@@ -8,3 +8,19 @@ variable "sample" {
 output "op" {
     value = var.sample
 }
+
+#whenever you want to print the variable in the group of words, you need to use the bash syntax
+output "ops" {
+    value = "My first msg is ${var.sample}"
+}
+
+# A variable can be accessed directly by caling  it as var.VariableName
+# In terraform there is no concept of single quotes. 
+
+variable "number" {
+    default = 100
+}
+
+output "op_number" {
+    value = var.number
+}
