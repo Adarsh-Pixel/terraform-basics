5b99fd92efac569d2d65e47996ad86576526bf4c
unknown <kumaradarsh921@gmail.com>
2024-02-28 18:14:58 +0530

    Git Auto push

--- 03-sample-vm/vm.tf
+++ 03-sample-vm/vm.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "app" {
   ami           = "ami-01f27c3753a7bf4fc"
-  instance_type = "t3.micro"
+  instance_type = "t3.medium"
 
   tags = {
     Name = "Terraform-Instance"
